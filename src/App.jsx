e3e5d63379abc7a8b77107b0bcbac7ebc6f7bfd1
TALITA ANTUNES <127903101+Talitaav@users.noreply.github.com>
2024-11-27 22:25:21 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import BuscaDeFilmes from './components/BuscaDeFilmes'
+
+function App() {
+
+
+  return (
+    <>
+      <BuscaDeFilmes />
+    </>
+  )
+}
+
+export default App
